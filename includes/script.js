ef67b7f26a73da6ab12e64e202b520f87ce26b40
Carib <cglasco.lovechevy@gmail.com>
2019-07-04 13:10:59 -0400

    added scroll to top fixed at bottom right

--- includes/script.js
+++ includes/script.js
@@ -14,3 +14,35 @@ hamburger.addEventListener('click', function() {
     }
 });
 // -----------------------------------------------------------
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+let toTop = document.getElementById('toTop');
+
+function scrollFunction() {
+let toTop = document.getElementById('toTop');
+if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    if (toTop.classList.contains('toTopFadeOut') === true) {
+        console.log('over 400 contains fade out');
+        toTop.classList.remove('toTopFadeOut');
+        toTop.classList.add('toTopFadeIn');
+    } else  {
+        console.log('over 400 does not contain fade in');
+        toTop.classList.add('toTopFadeIn');
+    }
+} else if (document.body.scrollTop < 400 || document.documentElement.scrollTop < 400) {
+    if (toTop.classList.contains('toTopFadeIn') === true) {
+        console.log('under 400 contains fade in');
+        toTop.classList.remove('toTopFadeIn');
+        toTop.classList.add('toTopFadeOut');
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+toTop.addEventListener('click', function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})}
