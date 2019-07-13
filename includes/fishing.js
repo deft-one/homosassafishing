4c2cb23ef237266a4f3ade3162209462627564bf
Carib <cglasco.lovechevy@gmail.com>
2019-07-13 09:19:55 -0400

    tidying up

--- includes/fishing.js
+++ includes/fishing.js
@@ -18,3 +18,26 @@ document.addEventListener('click', function(event) {
         event.target.classList.add('descripHide');
     }
 }, false);
+
+function scrollFunc() {
+    event.target.scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+document.addEventListener('click', function(event) {
+    function classSwap(callBack) {
+        if (event.target.classList.contains('the-boat')) {
+            if (event.target.classList.contains('gallerySizeUp')) {
+                event.target.classList.remove('gallerySizeUp');
+                event.target.classList.add('gallerySizeDn');
+            } else if (event.target.classList.contains('gallerySizeDn')) {
+                event.target.classList.remove('gallerySizeDn');
+                event.target.classList.add('gallerySizeUp');
+            } else {
+                event.target.classList.add('gallerySizeUp');
+            }
+        }
+        callBack();
+    }
+    classSwap(scrollFunc);
+    
+}, false);
